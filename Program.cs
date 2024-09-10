f78cc2940d3054c85aadfd37841ab2bc736fb9e8
Th1234wee <theara6574@gmail.com>
2024-09-10 16:20:48 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    static readonly List<String> AllValidChannels = new List<string> { "BE", "FE", "QA", "Urgent" };
+    private static void Main(string[] args)
+    {
+        List<String> selectedChannels = new List<string>();
+
+        while (true)
+        {
+            Console.Write("Input The recieve channel one by one(BE,FE,...etc.) or type done to finished : ");
+            string inputChannel = Console.ReadLine();
+
+            if (inputChannel == "done") break;
+            selectedChannels.Add(inputChannel.ToUpper());
+        }
+        Console.Write("What happen to the channel : ");
+        string statusMessage = Console.ReadLine();
+        string formattedChannel = FormatChannel(selectedChannels);
+        string result = GenerateFullNotificationTitle(formattedChannel, statusMessage);
+        Console.WriteLine($"Right Formatted input title : {result}");
+
+        List<string> allRecieveChannel = FilterValidRecieveChannel(selectedChannels);
+        if (allRecieveChannel.Any())
+        {
+            Console.WriteLine($"Recieve Channel : {string.Join(", ", allRecieveChannel)}");
+        }
+    }
+    static string GenerateFullNotificationTitle (string formattedChannel , string statusMessage)
+    {
+        return formattedChannel + " there is " + statusMessage ;
+    }
+    static string FormatChannel(List<String> selectedChannels)
+    {
+        return "[" + string.Join("][", selectedChannels) + "]";
+    }
+    static List<string> FilterValidRecieveChannel(List<String> selectedChannels)
+    {
+        return selectedChannels.Where(channel => AllValidChannels.Contains(channel)).ToList();
+    }
+}
---
+++ README.md
@@ -0,0 +1,57 @@
+# Notification Parser Console Application
+
+## Overview
+
+This program designed to parse the notification title and assign them to the valid different channel.
+
+##  Application Flow : 
+
+
+- **Flexible Input**: Allow the user to input channel name one by one , it likes we have user interface that we can choose or input channel one by one.
+- **Keeping right format** : All our inputted channel must be converted into the right format.
+- **Filter only the right one** : Even we input the invalid channel or wrong order , it will filter only the right channel.
+
+## Prerequsites
+
+- [.NET Core SDK](https://dotnet.microsoft.com/download) (Version 6.0 or later)
+- Text Editor or IDE (Visual Studio ,...)
+
+## Installation
+
+1. **Clone the repository**
+
+```bash
+git clone https://github.com/Theara123422/NotificationParser.git
+
+2. **Navigate to the project directory**
+
+```bash
+cd NotificationParser
+
+3. **Restore Dependencies**
+
+```bash
+dotnet restore
+
+4. **Build the app**
+
+```bash
+dotnet build
+
+## Usage
+
+	1. **Run the application (by CLI or open in text editor or IDE)**
+
+	```bash
+	dotnet run
+
+	2. **Example Output**
+
+		First look when you run the application , you will prompt to input the channel name one by one and message about notification. Here is an example output : 
+
+	 - [Image](ExampleOutput.png)
+
+## Contact
+Name : Pen Theara
+Email : theara6574@gmail.com
+
